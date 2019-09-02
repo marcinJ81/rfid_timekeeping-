2ce1f87422681ec2cdd253f281ceca7c6fa00507
Marcin Juranek <m.juranek@PGG.PL>
2019-09-02 09:59:43 +0200

    Starting application for timekeeping with RFID and usage MongoDB

---
+++ GIinterface/Program.cs
@@ -0,0 +1,19 @@
+﻿using mongoEngine;
+using System;
+
+namespace GIinterface
+{
+    class Program
+    {
+        private IMongoConnection imongoCon { get; set; }
+
+        public Program()
+        {
+            this.imongoCon = new MongoConnection();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ mongoEngine/SetConnectionToMongo.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace mongoEngine
+{
+    public interface IMongoConnection
+    {
+        MongoClient setConnocetion();
+    }
+
+    public class MongoConnection : IMongoConnection
+    {
+        public MongoClient setConnocetion()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            return client;
+        }
+    }
+}
