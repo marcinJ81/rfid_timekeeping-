e2ad6f7f758ce3ea5f0694770a13bb507e065116
Marcin Juranek <m.juranek@PGG.PL>
2020-01-16 10:33:01 +0100

    Remove inser methods from class ravenEngine.cs

--- ravenEngine/RavenEngine.cs
+++ ravenEngine/RavenEngine.cs
@@ -33,49 +33,4 @@ namespace ravenEngine
                 Urls = new[] { "http://localhost:8080" }
             }.Initialize();
     }
-
-    
-    public class RavenTest
-    {
-        public void InsertOne()
-        {
-            using (IDocumentSession session = DBConnetTest.Store.OpenSession())
-            {
-                var category = new Category
-                {
-                    Name = "Tags",
-                    id = 1
-                };
-
-                session.Store(category);
-
-                var product = new Product
-                {
-                    Name = "Laptop 2000",
-                    category_id = category
-                };
-
-                session.Store(product);
-                session.SaveChanges();
-            }
-        }
-        public string InsertSpecificDocument()
-        {
-            using (IDocumentSession session = DBConnect2.idb.OpenSession())
-            {
-                SProduct sProduct = new SProduct()
-                {
-                    Id = string.Empty, // if empty db generate GUID, case sensitivity
-                    Number = 1,
-                    Name = "Muscle Car",
-                    RegisterDate = DateTime.Now
-                };
-                session.Store(sProduct);
-                session.SaveChanges();
-                var guid = session.Advanced.GetDocumentId(sProduct);
-                return guid;
-            }
-        }
-
-    }
 }
