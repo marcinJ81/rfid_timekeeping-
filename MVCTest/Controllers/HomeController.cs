0be1da9b8f3731b439fdc05d98f793d2292808ee
Marcin Juranek <m.juranek@PGG.PL>
2020-02-06 11:27:45 +0100

    Add MVC project for show result from mongodb

---
+++ MVCTest/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using MVCTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Tag_model tagm = new Tag_model();
+            var result = tagm.GetTagForView();
+
+            return View(result);
+        }
+       
+    }
+}
